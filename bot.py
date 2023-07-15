1ab3b999f3099c30e0f74d71d752deeb7c27084b
Glutch <oliverwiljohansson@gmail.com>
2023-07-15 18:51:10 +0200

    Initial commit

---
+++ bot.py
@@ -0,0 +1,48 @@
+import time
+import pyautogui
+from tools import get_pixel, clear, press
+
+clear()
+
+while True:
+    health                      = get_pixel(1, "Health")
+    mana                        = get_pixel(2, "Mana")
+    target_health               = get_pixel(3, "TargetHP")
+    in_range                    = get_pixel(4, "In range")
+    combat                      = get_pixel(5, "Combat")
+    brain_freeze                = get_pixel(6, "Brain Freeze")
+    fingers_of_frost            = get_pixel(7, "fof")
+    slot1                       = get_pixel(8, "slot1")
+    slot2                       = get_pixel(9, "slot2")
+    slot3                       = get_pixel(10, "slot3")
+    slot4                       = get_pixel(11, "slot4")
+    slot5                       = get_pixel(12, "slot5")
+    slot6                       = get_pixel(13, "slot6")
+    slot7                       = get_pixel(14, "slot7")
+    slot8                       = get_pixel(15, "slot8")
+    slot9                       = get_pixel(16, "slot9")
+    slot10                      = get_pixel(17, "slot10")
+    slot11                      = get_pixel(18, "slot11")
+    slot12                      = get_pixel(19, "slot12")
+
+    # global_cd = slot1
+
+    # if not combat:
+    #     if frost_armor == 0:
+    #         press('e', 'shift')
+
+    #     if arcane_intellect == 0:
+    #         press('q', 'shift')
+
+    if target_health > 0 and in_range:
+        if fingers_of_frost:
+            press('2')
+            continue
+
+        if brain_freeze:
+            press('3')
+            continue
+
+    press('q')
+
+    
---
+++ tools.py
@@ -0,0 +1,65 @@
+import os
+import ctypes
+from PIL import Image
+import pyautogui
+
+class POINT(ctypes.Structure):
+    _fields_ = [("x", ctypes.c_long),("y", ctypes.c_long)]
+
+# Gotta take a screenshot and count how wide every pixel is
+PIXEL_SIZE_ON_SCREENSHOT = 3
+
+# Hanterar så att terminalen refreshar fint
+class WindowsConsoleOutput():
+    STD_OUTPUT_HANDLE = -11
+
+    def __init__(self):
+        self.handle = ctypes.windll.kernel32.GetStdHandle(self.STD_OUTPUT_HANDLE)
+
+    def write(self, x, y, text):
+        ctypes.windll.kernel32.SetConsoleCursorPosition(self.handle, ctypes.wintypes._COORD(x, y))
+        ctypes.windll.kernel32.WriteConsoleA(self.handle, text, 30, None, None)
+
+console_out = WindowsConsoleOutput()
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# Gör om rgb till number
+def rgb_to_number(rgb):
+    if all(isinstance(val, int) for val in rgb):
+        number = (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]
+    elif all(isinstance(val, float) for val in rgb):
+        number = (rgb[0] / 255.0) + (rgb[1] / 255.0 / 256.0) + (rgb[2] / 255.0 / 256.0 / 256.0)
+    else:
+        raise TypeError("Unsupported RGB type. Only int and float lists are supported.")
+    
+    return number
+
+# # Hämtar pixelns rgb
+def get_pixel_color(x, y):
+    hDC = ctypes.windll.user32.GetDC(0)
+    RGB = ctypes.windll.gdi32.GetPixel(hDC, x, y)
+    ctypes.windll.user32.ReleaseDC(0, hDC)
+    red = RGB & 255
+    green = (RGB >> 8) & 255
+    blue = (RGB >> 16) & 255
+    return [red, green, blue]
+
+# Hämtar pixelns rgb, parsear, printar den till terminalen
+def get_pixel(i, name):
+    pixel_color = get_pixel_color((i - 1) * PIXEL_SIZE_ON_SCREENSHOT, 0)
+    parsed_color = rgb_to_number(pixel_color)
+    text = f"{name}: {parsed_color}" + (' ' * 10)
+    console_out.write(0, i - 1, text)
+    return parsed_color
+
+
+def press(button, modifier = None):
+  if modifier:
+    pyautogui.keyDown(modifier)
+
+  pyautogui.press(button)
+
+  if modifier:
+    pyautogui.keyUp(modifier)
