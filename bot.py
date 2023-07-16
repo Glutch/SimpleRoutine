f84500ce7012225ec0e4c0224770d8f5d14dd34f
Kiyogitpy <139643727+Kiyogitpy@users.noreply.github.com>
2023-07-16 12:45:32 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,27 +16,28 @@ clear()
 while True:
     check_exit_hotkey()
     if get_active_window_title() == "World of Warcraft":
-        health = get_pixel(1, "Health")
-        mana = get_pixel(2, "Mana")
-        target_health = get_pixel(3, "TargetHP")
-        in_range = get_pixel(4, "In range")
-        combat = get_pixel(5, "Combat")
-        brain_freeze = get_pixel(6, "Brain Freeze")
-        fingers_of_frost = get_pixel(7, "fof")
-        slot1 = get_pixel(8, "slot1")
-        slot2 = get_pixel(9, "slot2")
-        slot3 = get_pixel(10, "slot3")
-        slot4 = get_pixel(11, "slot4")
-        slot5 = get_pixel(12, "slot5")
-        slot6 = get_pixel(13, "slot6")
-        slot7 = get_pixel(14, "slot7")
-        slot8 = get_pixel(15, "slot8")
-        slot9 = get_pixel(16, "slot9")
-        slot10 = get_pixel(17, "slot10")
-        slot11 = get_pixel(18, "slot11")
-        slot12 = get_pixel(19, "slot12")
-        debuff1 = get_pixel(20, "Debuff1")
-        isdead = get_pixel(21, "isdead?")
+        count = get_pixel(1, "Count")
+        health = get_pixel(2, "Health")
+        mana = get_pixel(3, "Mana")
+        target_health = get_pixel(4, "TargetHP")
+        in_range = get_pixel(5, "In range")
+        combat = get_pixel(6, "Combat")
+        brain_freeze = get_pixel(7, "Brain Freeze")
+        fingers_of_frost = get_pixel(8, "fof")
+        slot1 = get_pixel(9, "slot1")
+        slot2 = get_pixel(10, "slot2")
+        slot3 = get_pixel(11, "slot3")
+        slot4 = get_pixel(12, "slot4")
+        slot5 = get_pixel(13, "slot5")
+        slot6 = get_pixel(14, "slot6")
+        slot7 = get_pixel(15, "slot7")
+        slot8 = get_pixel(16, "slot8")
+        slot9 = get_pixel(17, "slot9")
+        slot10 = get_pixel(18, "slot10")
+        slot11 = get_pixel(19, "slot11")
+        slot12 = get_pixel(20, "slot12")
+        debuff1 = get_pixel(21, "Debuff1")
+        isdead = get_pixel(22, "isdead?")
 
         # if not combat:
             #     if frost_armor == 0:
@@ -49,7 +50,7 @@ while True:
             if debuff1 and slot9:
                 press('f')
                 continue
-
+            
         if target_health > 0 and in_range:
             if slot7:
                 press('7')
